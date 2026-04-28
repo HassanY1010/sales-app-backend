d5a27946003992a488f8900490af45095db83604
HassanY1010 <hassanmohammedbarafah836@gmail.com>
2026-04-28 08:50:46 +0300

    Initial commit - AI Executive Board project

--- src/finance/finance.module.ts
+++ src/finance/finance.module.ts
@@ -2,10 +2,12 @@ import { Module, Global } from '@nestjs/common';
 import { FinanceService } from './finance.service';
 import { FinanceController } from './finance.controller';
 import { DatabaseModule } from '../database/database.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { EventsModule } from '../events/events.module';
 
 @Global()
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, NotificationsModule, EventsModule],
   controllers: [FinanceController],
   providers: [FinanceService],
   exports: [FinanceService],
