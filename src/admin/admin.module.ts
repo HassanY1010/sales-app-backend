3d95b5250b68d61d5c1dd837c288096715c8b85d
HassanY1010 <hassanmohammedbarafah836@gmail.com>
2026-05-03 08:19:32 +0300

    Initial commit - AI Executive Board project

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { DatabaseModule } from '../database/database.module';
+import { EventsModule } from '../events/events.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, EventsModule],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [AdminService],
